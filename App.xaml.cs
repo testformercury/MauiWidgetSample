e0d32718ee839d4aaef08bfe4f3bcd8f805e606d
Udayavarman.Ravichandran <udayavarman.ravichandran@mercmarine.com>
2023-11-29 12:41:41 +0530

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet8WidgetSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8WidgetSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
